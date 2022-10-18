e6423b1fc127d37dfeac0ff4c855b23daecba89a
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-18 16:36:09 -0600

    Change text

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,9 +3,9 @@ import './Home.css'
 const Home = ({handleScroll}) => {
   return (
     <header id='home' className='home-wrapper'>
-      <h1>Hello, I'm Emili Kaiman.</h1>
-      <h1>I'm a Front-end engineer.</h1>
-      <a href='#about' onClick={() => { handleScroll()}}> View my work</a>
+      <h1>Hey, I'm Emili.</h1>
+      <h1>Front-end devloper.</h1>
+      <a href='#about' className='view-work-button'> View my work</a>
     </header>
   )
 }
