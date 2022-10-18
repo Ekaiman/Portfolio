8f98f7a176c609cf9a04653aebcd56e1377f7c63
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-18 12:53:52 -0600

    Create a home component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+const Home = () => {
+  return (
+    <header id='home' className='home-wrapper'>
+      <h1>Hello, I'm Emili Kaiman.</h1>
+      <h1>I'm a Front-end engineer.</h1>
+      <a href='#work'> View my work</a>
+    </header>
+  )
+}
+
+export default Home
