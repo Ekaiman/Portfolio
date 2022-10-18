fc5af25d1b1769910b80d1696c031862c4d40706
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-18 12:46:26 -0600

    Add styling

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+
+
+import NavBar from '../NavBar/NavBar';
+import Work from '../Work/Work';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Work/>
+    </div>
+  );
+}
+
+export default App;
