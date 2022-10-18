26d8ccbc33edc7b4be601db49a67d50f58749199
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-18 16:35:39 -0600

    Create contact component

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <section id='contact' className='contact-wrapper'>
+      <h1>Contact INFO</h1>
+    </section>
+  )
+}
+
+export default Contact
