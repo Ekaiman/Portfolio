5163be9b8fbe1abd3964a2b680c08b8af7584801
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-18 16:35:24 -0600

    Add item holders

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,6 +5,13 @@ const About = () => {
   return (
     <section id='about' className='about-wrapper'>
       <h1> THIS IS MY ABOUT ME</h1>
+      <img className='about-image' src='../../../../picture.png' />
+      <section className='about-items'>
+        <h2 className='item'>first</h2>
+        <h2 className='item'>second</h2>
+        <h2 className='item'>third</h2>
+        <h2 className='item'>fourth</h2>
+      </section>
     </section>
   )
 }
