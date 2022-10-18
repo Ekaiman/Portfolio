dcc02ade711d95bdadf3e5e206112f6ad8620dc6
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-18 12:46:12 -0600

    Create Work component

---
+++ src/Components/Work/Work.js
@@ -0,0 +1,11 @@
+import './Work.css'
+
+const Work = () => {
+  return (
+    <section className='work-wrapper'>
+      <h1>This section is about my work</h1>
+    </section>
+  )
+}
+
+export default Work
