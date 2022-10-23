c3bd57eb8f967c416e42ae677975166cc7d2015f
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-22 18:33:52 -0600

    Animation when in view

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,19 +1,65 @@
 
 import './About.css'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { useEffect } from 'react'
+
 const About = () => { 
 
+  const control = useAnimation()
+  const [ref, inView] = useInView()
+
+  const boxVariantLeft = {
+    visible: { x: 0, opacity: 1 },
+    hidden: { x: -200, opacity: 0 }
+  }
+
+  const boxVariantRight = {
+    visible: { x: 0, opacity: 1 },
+    hidden: { x: 200, opacity: 0 }
+  }
+
+  useEffect(() => {
+    console.log(inView)
+    if (inView) {
+      control.start('visible')
+    } 
+  }, [control, inView])
+
   return (
-    <section id='about' className='about-wrapper'>
-      <h1> THIS IS MY ABOUT ME</h1>
-      <img className='about-image' src='../../../../picture.png' />
-      <section className='about-items'>
-        <h2 className='item'>first</h2>
-        <h2 className='item'>second</h2>
-        <h2 className='item'>third</h2>
-        <h2 className='item'>fourth</h2>
-      </section>
-    </section>
+    <div id='about' className='about-screen'>
+      <h1> ABOUT</h1>
+
+      <motion.section
+        ref={ref}
+        variants={boxVariantLeft}
+        initial='hidden'
+        animate={control}
+        transition={{ ease: 'easeIn', duration: 1.5, delay: 0.3 }}
+        // viewport={{ once: false }}
+        // initial={{ opacity: 0, x: -150 }}
+        // whileInView={{ opacity: 1, x: 0 }}
+        // transition={{ type: "easeIn", duration: .15, delay: .50}}
+        className='about-wrapper'
+      >
+        <img className='about-image' src='../../../../picture.png' />
+        <motion.section
+          ref={ref}
+          variants={boxVariantRight}
+          initial='hidden'
+          animate={control}
+          transition={{ ease: 'easeIn', duration: 1.5, delay: 1 }}
+          className='about-items'
+        >
+          <h2 className='item1'>first</h2>
+          <h2 className='item2'>second</h2>
+          <h2 className='item3'>third</h2>
+          <h2 className='item4'>fourth</h2>
+        </motion.section>
+      </motion.section>
+    </div>
   )
 }
+
 
 export default About
