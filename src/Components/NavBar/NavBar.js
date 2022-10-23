3953cefe8fcc567cedcba0235e28b2d60d7d8bfd
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-23 13:44:21 -0600

    Add ClassNames

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -8,16 +8,24 @@ const NavBar = () => {
       <nav>
         <ul className='nav-items'>
           <li>
-            <a href='#home'>Home</a>
+            <a className='nav-button' href='#home'>
+              Home
+            </a>
           </li>
           <li>
-            <a href='#about'>About</a>
+            <a className='nav-button' href='#about'>
+              About
+            </a>
           </li>
           <li>
-            <a href='#work'>Portfolio</a>
+            <a className='nav-button' href='#work'>
+              Portfolio
+            </a>
           </li>
           <li>
-            <a href='#contact'>Contact</a>
+            <a className='nav-button' href='#contact'>
+              Contact
+            </a>
           </li>
         </ul>
       </nav>
