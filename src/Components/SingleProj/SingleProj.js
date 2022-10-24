04a0713e208a5117aa802deb016a7e2a9baf4388
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-24 11:00:31 -0600

    Add index change animation

--- src/Components/SingleProj/SingleProj.js
+++ src/Components/SingleProj/SingleProj.js
@@ -5,7 +5,7 @@ import { motion, useAnimation } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 
 
-const SingleProj = ({ title, img, gitHub, showModal, setSelectedProj, overview }) => {
+const SingleProj = ({ index, title, img, gitHub, showModal, setSelectedProj, overview }) => {
   const control = useAnimation()
   const [ref, inView] = useInView()
 
@@ -25,23 +25,29 @@ const SingleProj = ({ title, img, gitHub, showModal, setSelectedProj, overview }
 
   return (
     <>
-      
       <motion.div
         ref={ref}
         variants={boxVariantBottom}
         initial='hidden'
         animate={control}
-        transition={{ ease: 'easeIn', duration: 1, delay: 0.4 }} className='container'>
+        transition={{ ease: [1, -0.65, 0, 2.25], duration: 1, delay: index - .9 }}
+        className='container'
+      >
         <img className='project-image' src={img} />
         <div class='middle'>
           <button
             onClick={() => {
               showModal()
-              setSelectedProj({ title: title , img: img, gitHub: gitHub, overview: overview })
+              setSelectedProj({
+                title: title,
+                img: img,
+                gitHub: gitHub,
+                overview: overview
+              })
             }}
             class='text'
           >
-            SEE MORE
+            Learn More
           </button>
         </div>
       </motion.div>
