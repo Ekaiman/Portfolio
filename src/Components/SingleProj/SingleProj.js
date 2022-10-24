bba3c5fba13b728157c286bc777b4807f997f3f1
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-23 18:29:16 -0600

    Add project overview

--- src/Components/SingleProj/SingleProj.js
+++ src/Components/SingleProj/SingleProj.js
@@ -1,13 +1,44 @@
 import './SingleProj.css'
+import React, { useState } from 'react'
+import Modal from '../Modal/Modal'
 
-const SingleProj = ({ title, img, gitHub }) => {
+const SingleProj = ({ title, img, gitHub, showModal, setSelectedProj, overview }) => {
+  // const [showing, setShowing] = useState(false)
+
+  // const showModal = () => {
+  //   setShowing(true)
+  // }
+
+  // const closeModal = () => {
+  //   setShowing(false)
+  // }
+
   return (
-    <div className='container'>
-      <img className='project-image' src={img} />
-      <div class='middle'>
-        <button class='text'>SEE MORE</button>
+    <>
+      
+        {/* <Modal
+          title={title}
+          gitHub={gitHub}
+          show={showing}
+          closeModal={closeModal}
+          setShowing={setShowing}
+        /> */}
+      
+      <div className='container'>
+        <img className='project-image' src={img} />
+        <div class='middle'>
+          <button
+            onClick={() => {
+              showModal()
+              setSelectedProj({ title: title , img: img, gitHub: gitHub, overview: overview })
+            }}
+            class='text'
+          >
+            SEE MORE
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,41 +1,48 @@
 import SingleProj from '../SingleProj/SingleProj'
 import './Work.css'
+import Modal from '../Modal/Modal'
 
-const Work = () => {
+const Work = ({showModal, setSelectedProj, showing, closeModal, title, img, gitHub, overview }) => {
   const projects = [
     {
       title: 'Tic Tac Toe',
       img: '../../../../project_imgs/tictac.png',
-      gitHub: 'https://github.com/Ekaiman/tic-tac-toe'
+      gitHub: 'https://github.com/Ekaiman/tic-tac-toe',
+      overview: "Tic Tac Toe allows for users to play a game that automatically asses for a win or draw. Written in vanilla JS."
     },
     {
       title: 'Roll For Initiative',
       img: '../../../../project_imgs/dnd.png',
-      gitHub: 'https://github.com/jskomal/roll-for-initiative'
+      gitHub: 'https://github.com/jskomal/roll-for-initiative',
+      overview: "Roll For Initiative is a chance style combat game featuring the mythical monster and courageous herse from the Dungens and Dragons (DnD) universe. Written in React and TypeScript."
     },
     {
       title: 'Randcid Tomatillos',
       img: '../../../../project_imgs/rancid.png',
-      gitHub: 'https://github.com/Ekaiman/Rancid_Tomatillos'
+      gitHub: 'https://github.com/Ekaiman/Rancid_Tomatillos',
+      overview: ""
     },
     {
       title: 'Travel Tracker',
       img: '../../../../project_imgs/travel.png',
-      gitHub: 'https://github.com/Ekaiman/travelTracker'
+      gitHub: 'https://github.com/Ekaiman/travelTracker',
+      overview: ""
     },
     {
       title: 'DiverCity',
       img: '../../../../project_imgs/divercity.png',
-      gitHub: 'https://github.com/Capstone-LGBTQ-BIPOC/FE_DiverCity'
+      gitHub: 'https://github.com/Capstone-LGBTQ-BIPOC/FE_DiverCity',
+      overview: ""
     }
   ]
 
   const projectCard = projects.map(proj => {
-    return <SingleProj title={proj.title} img={proj.img} github={proj.gitHub} />
+    return <SingleProj setSelectedProj={setSelectedProj} showModal={showModal} title={proj.title} img={proj.img} gitHub={proj.gitHub} overview={proj.overview} />
   })
 
   return (
     <section id='work' className='work-wrapper'>
+      <Modal show={showing} closeModal={closeModal} title={title} img={img} gitHub={gitHub} overview={overview} />
       <h1>This section is about my work</h1>
       <div className='project-holder'>
         {projectCard}
