57908b6f56d85025cb0a62362de6539bf7f82597
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-24 11:00:51 -0600

    Find mouse coordinates

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,24 @@
+import { useState } from 'react'
 import './Home.css'
 
-const Home = ({handleScroll}) => {
+const Home = () => {
+
+  const [windowsize, setWindowSize] = useState({height: 0, width: 0})
+  const [mouseCoords, setMouseCoords] = useState({x: 0, y: 0})
+  
+  const handleMouseMove = (event) => { 
+    setMouseCoords({ x: event.clientX, y: event.clientY })
+    setWindowSize({height: window.innerHeight, width: window.innerWidth})
+  }
+  
   return (
-    <header id='home' className='home-wrapper'>
-      <h1>Hey, I'm Emili.</h1>
-      <h1>Front-end devloper.</h1>
-      <a href='#about' className='view-work-button'> View my work</a>
+    <header onMouseMove={(event) => {handleMouseMove(event) } } id='home' className='home-wrapper'>
+      <h1 className='home-text'>Hey, I'm <span style={{color: 'hotpink'}}>Emili.</span></h1>
+      <h1 className='home-text'>Front-end devloper.</h1>
+      <a href='#about' className='view-work-button'>
+        {' '}
+        View my work
+      </a>
     </header>
   )
 }
