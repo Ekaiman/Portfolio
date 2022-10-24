5ed39cf8f6b0290b3d95921df5585d12ad648c4a
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-23 19:40:20 -0600

    Contact formatting

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,9 +2,19 @@ import './Contact.css'
 
 const Contact = () => {
   return (
-    <section id='contact' className='contact-wrapper'>
-      <h1>Contact INFO</h1>
-    </section>
+    <>
+      <section id='hexagon'>
+        <section id='contact' className='contact-wrapper'>
+          <div className='space'></div>
+          <h1>Contact INFO</h1>
+          <footer className='contact-links'>
+            <button>github</button>
+            <button>linkedin</button>
+            <button>email</button>
+          </footer>
+        </section>
+      </section>
+    </>
   )
 }
 
