2483952409b7bc5a5c4fd3aebe25336b5f207065
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-23 20:20:48 -0600

    Add icons href

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,11 +6,34 @@ const Contact = () => {
       <section id='hexagon'>
         <section id='contact' className='contact-wrapper'>
           <div className='space'></div>
-          <h1>Contact INFO</h1>
+          <h1>CONTACT</h1>
+          <p>Have a question or want to work together?</p>
+          <p>Shoot me an <a href='mailto:emilikaiman@gmail.com'>email.</a></p>
           <footer className='contact-links'>
-            <button>github</button>
-            <button>linkedin</button>
-            <button>email</button>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://github.com/Ekaiman'
+            >
+              <img
+                className='icons'
+                src='../../../../social-icons/Octocat.png'
+              />
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='https://www.linkedin.com/in/emilikaiman/'
+            >
+              <img className='icons' src='../../../../social-icons/li.png' />
+            </a>
+            <a
+              target='_blank'
+              rel='noopener noreferrer'
+              href='mailto:emilikaiman@gmail.com'
+            >
+              <img className='icons' src='../../../../social-icons/email.png' />
+            </a>
           </footer>
         </section>
       </section>
