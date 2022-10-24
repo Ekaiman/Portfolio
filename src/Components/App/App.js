52cf84f67e96fb0a3aecdfcd7a543599fe84552e
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-24 11:01:06 -0600

    Import components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,48 @@
+import NavBar from '../NavBar/NavBar'
+import Work from '../Work/Work'
+import Home from '../Home/Home'
+import About from '../About/About'
+import { useState, useEffect } from 'react'
+import './App.css'
+import Contact from '../Contact/Contact'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
-
-import NavBar from '../NavBar/NavBar';
-import Work from '../Work/Work';
-import './App.css';
+// import Modal from '../Modal/Modal'
 
 function App() {
+  const [showing, setShowing] = useState(false)
+  const [selectedProj, setSelectedProj] = useState({title: '', img: '', gitHub: '', overview: ''})
+
+  const showModal = () => {
+    setShowing(true)
+  }
+
+  const closeModal = () => {
+    setShowing(false)
+    setSelectedProj({ title: '', img: '', gitHub: '', overview: '' })
+  }
+
   return (
-    <div className="App">
-      <NavBar/>
-      <Work/>
+    <div className='app'>
+      <Home />
+      {/* {scroll && <NavBar setScroll={setScroll} />} */}
+      <NavBar />
+      <About />
+      {/* (<Modal show={showing} closeModal={closeModal} title={selectedProj.title} img={selectedProj.img} github ={selectedProj.gitHub} /> */}
+      <Work
+        showModal={showModal}
+        setSelectedProj={setSelectedProj}
+        showing={showing}
+        closeModal={closeModal}
+        title={selectedProj.title}
+        img={selectedProj.img}
+        gitHub={selectedProj.gitHub}
+        overview={selectedProj.overview}
+      />
+      <Contact />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
