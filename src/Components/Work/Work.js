571628a6942be9c818375170309f248cb0bb5110
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-10-29 13:12:30 -0600

    Change project order

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -27,48 +27,54 @@ const Work = ({
 
   const projects = [
     {
-      title: 'Tic Tac Toe',
-      img: '../../../../project_imgs/tictac.png',
-      gitHub: 'https://github.com/Ekaiman/tic-tac-toe',
-      overview:
-        'Tic Tac Toe allows for users to play a game that automatically asses for a win or draw. Written in vanilla JS.',
-      lang: 'Vanilla JS'
-    },
-    {
+      id: 2,
       title: 'Roll For Initiative',
       img: '../../../../project_imgs/dnd.png',
       gitHub: 'https://github.com/jskomal/roll-for-initiative',
       overview:
-        'Roll For Initiative is a chance style combat game featuring the mythical monster and courageous herse from the Dungens and Dragons (DnD) universe. Written in React and TypeScript.',
+      'Roll For Initiative is a chance style combat game featuring the mythical monster and courageous herse from the Dungens and Dragons (DnD) universe. Written in React and TypeScript.',
       lang: 'React/TypeScript'
     },
     {
-      title: 'Randcid Tomatillos',
-      img: '../../../../project_imgs/rancid.png',
-      gitHub: 'https://github.com/Ekaiman/Rancid_Tomatillos',
-      overview: '',
+      id: 1,
+      title: 'DiverCity',
+      img: '../../../../project_imgs/divercity.png',
+      gitHub: 'https://github.com/Capstone-LGBTQ-BIPOC/FE_DiverCity',
+      overview: 'Divercity',
       lang: 'React'
     },
     {
+      id: 3,
+      title: 'Tic Tac Toe',
+      img: '../../../../project_imgs/tictac.png',
+      gitHub: 'https://github.com/Ekaiman/tic-tac-toe',
+      overview:
+        'Tic Tac Toe allows for users to play a game that automatically asses for a win or draw. Written in vanilla JavaScript.',
+      lang: 'Vanilla JS'
+    },
+    {
+      id: 5,
+      title: 'Randcid Tomatillos',
+      img: '../../../../project_imgs/rancid.png',
+      gitHub: 'https://github.com/Ekaiman/Rancid_Tomatillos',
+      overview:
+        'Rancid tomatillos was a project built to learn React and dynamic routing. ',
+      lang: 'React'
+    },
+    {
+      id: 4,
       title: 'Travel Tracker',
       img: '../../../../project_imgs/travel.png',
       gitHub: 'https://github.com/Ekaiman/travelTracker',
       overview: '',
       lang: 'Vanilla JS'
-    },
-    {
-      title: 'DiverCity',
-      img: '../../../../project_imgs/divercity.png',
-      gitHub: 'https://github.com/Capstone-LGBTQ-BIPOC/FE_DiverCity',
-      overview: '',
-      lang: 'React'
     }
   ]
 
   const projectCard = projects.map((proj, index) => {
     return (
       <SingleProj
-        ref={ref}
+        
         inView={inView}
         index={index}
         setSelectedProj={setSelectedProj}
@@ -78,6 +84,8 @@ const Work = ({
         gitHub={proj.gitHub}
         overview={proj.overview}
         control={control}
+        lang={proj.lang}
+        key={proj.id}
       />
     )
   })
@@ -92,6 +100,7 @@ const Work = ({
         gitHub={gitHub}
         overview={overview}
       />
+
       <h1 className='section-title'>PROJECTS</h1>
       <div className='project-holder'>{projectCard}</div>
     </motion.section>
