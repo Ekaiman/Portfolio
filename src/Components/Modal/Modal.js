fbac1825ec0179109798dfefb96d6bc82e0c954a
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-11-29 12:21:01 -0700

    Create buttons and functionality for changing img

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -2,20 +2,64 @@ import './Modal.css'
 import { motion } from 'framer-motion'
 
 const Modal = ({ show, title, img, gitHub, closeModal, overview }) => {
+  let count = 0
+
+  const nextImage = () => { 
+    count++
+    console.log(img.length)
+    console.log(img)
+    if (img.length > count) {
+      document.querySelector('.image-in-modal').src = img[count]
+      console.log('next')
+    } else {
+      count = 0
+      console.log('begining')
+      document.querySelector('.image-in-modal').src = img[count]
+    }
+  }
+
+  const backImage = () => { 
+    count--
+    if (count >= 0) {
+      document.querySelector('.image-in-modal').src = img[count]
+      console.log('next')
+    } else {
+      count = img.length -1
+      console.log(count)
+      document.querySelector('.image-in-modal').src = img[count]
+    }
+    
+  }
+
   return (
     show && (
       <motion.section
         className='modal-outside'
         key='modal'
-        initial={{ opacity: 0, scale: .7}}
+        initial={{ opacity: 0, scale: 0.7 }}
         animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: .7 }}
+        exit={{ opacity: 0, scale: 0.7 }}
       >
         <div className='modal-holder' onClick={e => e.stopPropagation()}>
-          <div className='modal-image'>
-            <img className='image-in-modal' src={img} />
-          </div>
+          {/* <div className='modal-image'> */}
+          <img className='image-in-modal' src={img[0]} alt={title} />
+          {/* </div> */}
           <section className='overview-holder'>
+            <button
+              onClick={() => {
+                nextImage()
+              }}
+            >
+              NEXT
+            </button>
+            <button
+              onClick={() => {
+                backImage()
+              }}
+            >
+              BACK
+            </button>
+
             <h1 className='title'>{title}</h1>
             <hr />
             <p className='overview'>{overview}</p>
