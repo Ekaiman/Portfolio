e3f71a0dc808859afe2a70526b108d29e264514c
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-11-29 14:49:55 -0700

    Conditonal button rendering and description

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,10 +1,11 @@
 import './Modal.css'
 import { motion } from 'framer-motion'
+import { useState, useEffect } from 'react'
 
-const Modal = ({ show, title, img, gitHub, closeModal, overview }) => {
+const Modal = ({ show, title, img, gitHub, closeModal, overview, description }) => {
   let count = 0
 
-  const nextImage = () => { 
+  const nextImage = () => {
     count++
     console.log(img.length)
     console.log(img)
@@ -18,17 +19,16 @@ const Modal = ({ show, title, img, gitHub, closeModal, overview }) => {
     }
   }
 
-  const backImage = () => { 
+  const backImage = () => {
     count--
     if (count >= 0) {
       document.querySelector('.image-in-modal').src = img[count]
       console.log('next')
     } else {
-      count = img.length -1
+      count = img.length - 1
       console.log(count)
       document.querySelector('.image-in-modal').src = img[count]
     }
-    
   }
 
   return (
@@ -41,44 +41,54 @@ const Modal = ({ show, title, img, gitHub, closeModal, overview }) => {
         exit={{ opacity: 0, scale: 0.7 }}
       >
         <div className='modal-holder' onClick={e => e.stopPropagation()}>
-          {/* <div className='modal-image'> */}
+          {/* <div className='image-holder'> */}
           <img className='image-in-modal' src={img[0]} alt={title} />
           {/* </div> */}
           <section className='overview-holder'>
-            <button
-              onClick={() => {
-                nextImage()
-              }}
-            >
-              NEXT
-            </button>
-            <button
-              onClick={() => {
-                backImage()
-              }}
-            >
-              BACK
-            </button>
+            {img.length > 1 && (
+              <div className='next-back-holder'>
+                <button
+                  className='back-button'
+                  onClick={() => {
+                    backImage()
+                  }}
+                >
+                  BACK
+                </button>
 
-            <h1 className='title'>{title}</h1>
-            <hr />
-            <p className='overview'>{overview}</p>
-            <div className='button-holder'>
-              <a
-                className='gitHub-button'
-                target='_blank'
-                rel='noopener noreferrer'
-                href={gitHub}
-              >
-                {' '}
-                View GitHub
-              </a>
-              <button
-                className='close'
-                onClick={() => {
-                  closeModal()
-                }}
-              ></button>
+                <button
+                  className='next-button'
+                  onClick={() => {
+                    nextImage()
+                  }}
+                >
+                  NEXT
+                </button>
+              </div>
+            )}
+
+            <div className='modal-content-holder'>
+              <h1 className='title'>{title}</h1>
+              <h2 className='description'>{description}</h2>
+              <hr />
+              <p className='overview'>{overview}</p>
+              <div className='button-holder'>
+                <a
+                  className='gitHub-button'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href={gitHub}
+                >
+                  {' '}
+                  View GitHub
+                </a>
+                <button
+                  className='close'
+                  onClick={() => {
+                    closeModal()
+                  }}
+                ></button>
+              </div>
             </div>
           </section>
         </div>
