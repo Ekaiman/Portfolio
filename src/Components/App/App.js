eab44b51bda68c3d7a2b202797195b1eb484907d
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-12-02 18:06:36 -0700

    Fix typos add abouts

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -62,7 +62,7 @@ function App() {
       ],
       gitHub: 'https://github.com/jskomal/roll-for-initiative',
       overview:
-        'Roll For Initiative is a chance style combat game featuring the mythical monster and courageous herse from the Dungens and Dragons (DnD) universe. Written in React and TypeScript.',
+        'Roll For Initiative is a chance style combat game featuring the mythical monster and courageous heroes from the Dungens and Dragons (DnD) universe. Written in React and TypeScript this group project was designed with mobile in mind.',
       lang: 'React / TypeScript',
       description: 'Game'
     },
@@ -76,7 +76,7 @@ function App() {
       ],
       gitHub: 'https://github.com/Capstone-LGBTQ-BßIPOC/FE_DiverCity',
       overview:
-        'DiverCity is a resource to browse and discover LGBTQ+ and BIPOC owned businesses. This application with built using React, React Context API for global state management and Cypress for End-to-End testing automated with CircleCI.',
+        'DiverCity is a resource to browse and discover LGBTQ+ and BIPOC owned businesses. This application was built using React, React Context API for global state management and Cypress for End-to-End testing automated with CircleCI.',
       lang: 'React / Ruby on Rails',
       description: 'Busniess Guide'
     },
@@ -90,7 +90,7 @@ function App() {
       ],
       gitHub: 'https://github.com/Ekaiman/tic-tac-toe',
       overview:
-        'Tic Tac Toe allows for users to play a game that automatically asses for a win or draw. Written in vanilla JavaScript.',
+        'Tic Tac Toe allows for two users to play a game that automatically assess for a win or draw. Written in vanilla JavaScript and without looking at other tutorials, this was my first solo project.',
       lang: 'Vanilla JS',
       description: 'Game'
     },
@@ -104,7 +104,7 @@ function App() {
       ],
       gitHub: 'https://github.com/Ekaiman/Rancid_Tomatillos',
       overview:
-        'Rancid tomatillos was a project built to learn React, Cypress, and dynamic routing. ',
+        'Rancid tomatillos was a project built to learn React, Cypress, and dynamic routing. This interface uses a mock API and was designed to reflect a Movie Ratings service.',
       lang: 'React',
       description: 'Movie Ratings'
     },
@@ -185,21 +185,21 @@ function App() {
         elementInView={elementInView}
       />
       {/* (<Modal show={showing} closeModal={closeModal} title={selectedProj.title} img={selectedProj.img} github ={selectedProj.gitHub} /> */}
-        <Work
-          showModal={showModal}
-          setSelectedProj={setSelectedProj}
-          showing={showing}
-          closeModal={closeModal}
-          title={selectedProj.title}
-          img={selectedProj.img}
-          gitHub={selectedProj.gitHub}
-          overview={selectedProj.overview}
-          setElementInView={setElementInView}
-          elementInView={elementInView}
-          projects={projects}
-          description={selectedProj.description}
-        />
-    
+      <Work
+        showModal={showModal}
+        setSelectedProj={setSelectedProj}
+        showing={showing}
+        closeModal={closeModal}
+        title={selectedProj.title}
+        img={selectedProj.img}
+        gitHub={selectedProj.gitHub}
+        overview={selectedProj.overview}
+        setElementInView={setElementInView}
+        elementInView={elementInView}
+        projects={projects}
+        description={selectedProj.description}
+      />
+
       <Contact
         setElementInView={setElementInView}
         elementInView={elementInView}
