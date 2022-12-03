94eb006b8c3ca823744685e7a7109357907c29b9
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-12-03 13:43:32 -0700

    Add radio to change bio length

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,6 +8,17 @@ const About = ({ setElementInView, elementInView }) => {
   const control = useAnimation()
   const [ref, inView] = useInView()
   const [ref1, inView1] = useInView()
+  const [bioLength, setBioLength] = useState('mid')
+
+  const emiliAbout = {
+    short: 'Im Emili: creative thinker and problem solver',
+    shortMid:
+      "I'm Emili: creative thinker gone Front-end Engineer.  I know JavaScript and React really well.",
+    mid: "I'm Emili: creative thinker, challenge seeker and Front-end Engineer. I thrive in environments that require critical thinking and problem solving. I know JavaScript, React, TypeScript and think E2E and user flow testing is important.",
+    midLong:
+      "I'm Emili: artist, and creative thinker gone Front-end Engineer. As someone who actively seeks out challenges, I'm excited to be in a career that promotes life long learning and endless opportunities for growth. I thrive in environments that require critical thinking and problem solving. I've spent the last year learning front-end development and specialize in JavaScript, React, TypeScript and see importance in both end-to-end and user flow testing. I'm located in Denver, CO.",
+    long: "I'm Emili: outdoor enthusiast, artist, and creative thinker gone Front-end Engineer. As someone who actively seeks out challenges, I'm excited to be in a career that promotes life long learning and endless opportunities for growth. I thrive in environments that require critical thinking and problem solving. I've spent the last year learning front-end development and specialize in JavaScript, React, TypeScript and see importance in both end-to-end and user flow testing. I'm located in Denver, CO and in my spare time you'll find me in the outdoors finding ways to get my endorphins pumping, in a ceramics studio, or curled up cuddling my cat named baby."
+  }
 
   const boxVariantLeft = {
     visible: { x: 0, opacity: 1 },
@@ -59,23 +70,63 @@ const About = ({ setElementInView, elementInView }) => {
           transition={{ ease: [1, -0.65, 0, 2.25], duration: 2, delay: 0.3 }}
           className='about-items'
         >
-          <h2 className='about-words'>
-            I'm Emili: outdoor enthusiast, artist, and creative thinker gone
-            Front-End Engineer.
-          </h2>
-          <h3 className='about-words'>
-            As someone who actively seeks out challenges, I'm excited to be in a
-            career that promotes life long learning and endless opporunities for
-            growth. I thrive in enviorments that require critical thinking and
-            problem solving. I've spent the last year learning front-end
-            development, and specialize in JavaScript, React, TypeScript and see
-            the importance in both end-to-end testing and user flow testing.
-          </h3>
-          <h3 className='about-words'>
-            I'm located in Denver, CO and in my spare time you'll find me in the
-            outdoors finding ways to get my endorphins pumping, or curled up
-            cuddling my cat named Baby.
-          </h3>
+          <p>Adjust bio length:</p>
+          <div className='bio-length'>
+            <div className='radio-buttons'>
+              <input
+                type='radio'
+                value='short'
+                className='short'
+                name='length'
+                onClick={() => {
+                  setBioLength('short')
+                }}
+              />
+              <input
+                type='radio'
+                value='shortMid'
+                className='shortMid'
+                name='length'
+                onClick={() => {
+                  setBioLength('shortMid')
+                }}
+              />
+              <input
+                type='radio'
+                value='mid'
+                className='mid'
+                name='length'
+                defaultChecked={true}
+                onClick={() => {
+                  setBioLength('mid')
+                }}
+              />
+              <input
+                type='radio'
+                value='midLong'
+                className='midLong'
+                name='length'
+                onClick={() => {
+                  setBioLength('midLong')
+                }}
+              />
+              <input
+                type='radio'
+                value='long'
+                className='long'
+                name='length'
+                onClick={() => {
+                  setBioLength('long')
+                }}
+              />
+            </div>
+            <div className='short-long'>
+              <p>shortest</p>
+              <p>longest</p>
+            </div>
+          </div>
+
+          <h2 className='about-words'>{emiliAbout[bioLength]}</h2>
         </motion.section>
       </motion.section>
     </div>
