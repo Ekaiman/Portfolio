62b14893d763c8191569017113adfbbbb366ffcf
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-12-04 16:39:35 -0700

    Add lets connect animation

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,6 +4,8 @@ import { useInView } from 'react-intersection-observer'
 import { useEffect } from 'react'
 import { useRef, useState, useCallback } from 'react'
 // import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const About = ({ setElementInView, elementInView, play }) => {
   const control = useAnimation()
@@ -73,82 +75,113 @@ const About = ({ setElementInView, elementInView, play }) => {
           transition={{ ease: [1, -0.65, 0, 2.25], duration: 2, delay: 0.3 }}
           className='about-items'
         >
-          <p className='adjust-bio-length'>Adjust bio length:</p>
-          <div className='bio-length'>
-            <div className='radio-buttons'>
-              <input
-                type='radio'
-                value='short'
-                className='radio-button'
-                name='length'
-                // onMouseEnter={() => {
-                // }}
-                onClick={() => {
-                  setBioLength('short')
-                }}
-                onMouseEnter={() => {
-                  play()
-                }}
-              />
-              <input
-                type='radio'
-                value='shortMid'
-                className='radio-button'
-                name='length'
-                onClick={() => {
-                  setBioLength('shortMid')
-                }}
-                onMouseEnter={() => {
-                  play()
-                }}
-              />
-              <input
-                type='radio'
-                value='mid'
-                className='radio-button'
-                name='length'
-                defaultChecked={true}
-                onClick={() => {
-                  setBioLength('mid')
-                }}
-                onMouseEnter={() => {
-                  play()
-                }}
-              />
-              <input
-                type='radio'
-                value='midLong'
-                className='radio-button'
-                name='length'
-                onClick={() => {
-                  setBioLength('midLong')
-                }}
-                onMouseEnter={() => {
-                  play()
-                }}
-              />
-              <input
-                type='radio'
-                value='long'
-                className='radio-button'
-                name='length'
-                onClick={() => {
-                  setBioLength('long')
-                }}
-                onMouseEnter={() => {
-                  play()
-                }}
-              />
+          <section className='bio-radio-holder'>
+            <div>
+              <p className='adjust-bio-length'>Adjust bio length:</p>
+              <div className='bio-length'>
+                <div className='radio-buttons'>
+                  <input
+                    type='radio'
+                    value='short'
+                    className='radio-button'
+                    name='length'
+                    // onMouseEnter={() => {
+                    // }}
+                    onClick={() => {
+                      setBioLength('short')
+                    }}
+                    onMouseEnter={() => {
+                      play()
+                    }}
+                  />
+                  <input
+                    type='radio'
+                    value='shortMid'
+                    className='radio-button'
+                    name='length'
+                    onClick={() => {
+                      setBioLength('shortMid')
+                    }}
+                    onMouseEnter={() => {
+                      play()
+                    }}
+                  />
+                  <input
+                    type='radio'
+                    value='mid'
+                    className='radio-button'
+                    name='length'
+                    defaultChecked={true}
+                    onClick={() => {
+                      setBioLength('mid')
+                    }}
+                    onMouseEnter={() => {
+                      play()
+                    }}
+                  />
+                  <input
+                    type='radio'
+                    value='midLong'
+                    className='radio-button'
+                    name='length'
+                    onClick={() => {
+                      setBioLength('midLong')
+                    }}
+                    onMouseEnter={() => {
+                      play()
+                    }}
+                  />
+                  <input
+                    type='radio'
+                    value='long'
+                    className='radio-button'
+                    name='length'
+                    onClick={() => {
+                      setBioLength('long')
+                    }}
+                    onMouseEnter={() => {
+                      play()
+                    }}
+                  />
+                </div>
+                <div className='short-long'>
+                  <p>shortest</p>
+                  <p>longest</p>
+                </div>
+              </div>
             </div>
-            <div className='short-long'>
-              <p>shortest</p>
-              <p>longest</p>
-            </div>
-          </div>
+          </section>
           <h2 className='intro'>I'm Emili:</h2>
           <h2 className='about-words intro'>{emiliAbout[bioLength]}</h2>
         </motion.section>
       </motion.section>
+      <motion.a
+        ref={ref}
+        variants={boxVariantRight}
+        initial='hidden'
+        animate={control}
+        transition={{ ease: [1, -0.65, 0, 2.25], duration: 2, delay: 0.3 }}
+        className='about-me-icons'
+        href='#contact'
+      >
+        <p className='contact-connect'>LETS CONNECT!</p>
+        <motion.div
+          animate={{
+            // scale: [1, 1.2, 1.2, 1, 1],
+            translateY: [0, 0, 10, 10, 0]
+            // borderRadius: ['0%', '0%', '50%', '50%', '0%']
+          }}
+          transition={{
+            duration: 1,
+            ease: 'easeInOut',
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: Infinity,
+            repeatDelay: 1
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowDown} />
+        </motion.div>
+      </motion.a>
     </div>
   )
 }
